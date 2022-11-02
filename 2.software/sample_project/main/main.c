89cfcdf211a45639d4a434ab91cceb85742d2941
Joseph-dot <2561942814@qq.com>
2022-11-02 22:43:33 +0800

    update

--- 2.software/sample_project/main/main.c
+++ 2.software/sample_project/main/main.c
@@ -1,9 +1,29 @@
 #include <stdio.h>
 #include "driver/gpio.h"
+#include "driver/i2c.h"
 
-
+/*      This is PW1555 GPIO        */
 #define PW1555_EN 4
 #define GPIO_OUTPUT_PIN_SEL (1ULL<<PW1555_EN)
+
+/*      This is BQ27441 GPIO        */
+#define BQ27441_SCL_IO      22
+#define BQ27441_SDA_IO      21
+#define BQ27441_BIN_IO      23
+#define BQ27441_GPOUT_IO    19
+
+#define I2C_MASTER_NUM 1
+#define I2C_MASTER_FREQ_HZ 100000
+#define I2C_MASTER_TX_BUF_DISABLE 0
+#define I2C_MASTER_RX_BUF_DISABLE 0
+
+#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
+#define ACK_CHECK_EN 0x1                        /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS 0x0                       /*!< I2C master will not check ack from slave */
+#define ACK_VAL 0x0                             /*!< I2C ack value */
+#define NACK_VAL 0x1                            /*!< I2C nack value */
+
 
 void gpio_init(void)
 {
@@ -18,6 +38,73 @@ void gpio_init(void)
     gpio_config(&io_conf);
 
     gpio_set_level(PW1555_EN,0);
+}
+
+
+int i2c_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf;
+
+    conf.mode = I2C_MODE_MASTER,
+    conf.sda_io_num = BQ27441_SDA_IO,
+    conf.sda_pullup_en = GPIO_PULLUP_ENABLE,
+    conf.scl_io_num = BQ27441_SDA_IO,
+    conf.scl_pullup_en = GPIO_PULLUP_ENABLE,
+    conf.master.clk_speed = I2C_MASTER_FREQ_HZ,
+
+    i2c_param_config(i2c_master_port,&conf);
+    return i2c_driver_install(i2c_master_port,conf.mode,I2C_MASTER_RX_BUF_DISABLE,I2C_MASTER_TX_BUF_DISABLE,0);
+}
+
+/**
+ @brief I2C写数据函数
+ @param slaveAddr -[in] 从设备地址
+ @param regAddr -[in] 寄存器地址
+ @param pData -[in] 写入数据
+ @param dataLen -[in] 写入数据长度
+ @return 错误码
+*/
+int I2C_WriteData(uint8_t slaveAddr, uint8_t regAddr, uint8_t *pData, uint16_t dataLen)
+{
+    int ret;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (slaveAddr << 1) | WRITE_BIT, ACK_CHECK_EN);
+    if(NULL != regAddr)
+    {
+        i2c_master_write_byte(cmd, regAddr, ACK_CHECK_EN);
+    }
+    i2c_master_write(cmd, pData, dataLen, ACK_CHECK_EN);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(1, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
+}
+
+/**
+ @brief I2C读数据函数
+ @param slaveAddr -[in] 从设备地址
+ @param regAddr -[in] 寄存器地址
+ @param pData -[in] 读出数据
+ @param dataLen -[in] 读出数据长度
+ @return 错误码
+*/
+int I2C_ReadData(uint8_t slaveAddr, uint8_t regAddr, uint8_t *pData, uint16_t dataLen)
+{
+    int ret;
+    i2c_cmd_handle_t cmd = i2c_cmd_link_create();
+    i2c_master_start(cmd);
+    i2c_master_write_byte(cmd, (slaveAddr << 1) | READ_BIT, ACK_CHECK_EN);
+    if(NULL != regAddr)
+    {
+        i2c_master_write_byte(cmd, regAddr, ACK_CHECK_EN);
+    }
+    i2c_master_read(cmd, pData, dataLen, ACK_VAL);
+    i2c_master_stop(cmd);
+    ret = i2c_master_cmd_begin(1, cmd, 1000 / portTICK_RATE_MS);
+    i2c_cmd_link_delete(cmd);
+    return ret;
 }
 
 
